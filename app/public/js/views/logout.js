36188cea197ba01d85f0e59cf21a666e8a88c354
Chris Wille <chris@christopherwille.com>
2018-02-07 11:54:54 -0500

    removed extra files

--- app/public/js/views/logout.js
+++ app/public/js/views/logout.js
@@ -1,18 +1,22 @@
-function logout() {
-    console.log("logout pressed")
-    $.ajax({
-        url: "/logout",
-        type: "POST",
-        data: {
-            logout: true
-        },
-        success: function(data) {
-            console.log("hup");
-            location.reload();
-            //that.showLockedAlert('You are now logged out.<br>Redirecting you back to the homepage.');
-        },
-        error: function(jqXHR) {
-            console.log(jqXHR.responseText + ' :: ' + jqXHR.statusText);
-        }
+$(document).ready(function() {
+
+    $("#btn-logout").click(() => {
+        console.log("click")
+        $.ajax({
+            url: "/logout",
+            type: "POST",
+            data: {
+                logout: true
+            },
+            success: function(data) {
+                console.log("hup");
+                location.reload();
+                //that.showLockedAlert('You are now logged out.<br>Redirecting you back to the homepage.');
+            },
+            error: function(jqXHR) {
+                console.log(jqXHR.responseText + ' :: ' + jqXHR.statusText);
+            }
+        });
     });
-}
+
+});
