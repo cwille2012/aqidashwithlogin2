cdfeb0d3bea6818e507440bcaac6474cef5a8a03
Chris Wille <chris@christopherwille.com>
2018-02-07 11:42:35 -0500

    removed document ready

--- app/public/js/views/logout.js
+++ app/public/js/views/logout.js
@@ -1,22 +1,18 @@
-$(document).ready(function() {
-
-    function logout() {
-        console.log("logout pressed")
-        $.ajax({
-            url: "/logout",
-            type: "POST",
-            data: {
-                logout: true
-            },
-            success: function(data) {
-                console.log("hup");
-                location.reload();
-                //that.showLockedAlert('You are now logged out.<br>Redirecting you back to the homepage.');
-            },
-            error: function(jqXHR) {
-                console.log(jqXHR.responseText + ' :: ' + jqXHR.statusText);
-            }
-        });
-    }
-
-});
+function logout() {
+    console.log("logout pressed")
+    $.ajax({
+        url: "/logout",
+        type: "POST",
+        data: {
+            logout: true
+        },
+        success: function(data) {
+            console.log("hup");
+            location.reload();
+            //that.showLockedAlert('You are now logged out.<br>Redirecting you back to the homepage.');
+        },
+        error: function(jqXHR) {
+            console.log(jqXHR.responseText + ' :: ' + jqXHR.statusText);
+        }
+    });
+}
