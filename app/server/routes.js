0449605d439b732019ed599c64c5e796dc729ea0
Chris Wille <chris@christopherwille.com>
2018-02-08 00:52:02 -0500

    sensors added

--- app/server/routes.js
+++ app/server/routes.js
@@ -220,12 +220,11 @@ module.exports = function(app) {
 
     // view & delete accounts //
 
-    app.get('/print', function(req, res) {
+    app.get('/users', function(req, res) {
         if (req.session.user == null) {
             res.redirect('/');
         } else {
             AM.getAllRecords(function(e, accounts) {
-                //res.render('print', { title: 'Account List', accts: accounts });
                 res.writeHead(200, { 'Content-Type': 'application/json' });
                 for (var account in accounts) {
                     accounts[account]['pass'] = 'hidden';
@@ -251,13 +250,39 @@ module.exports = function(app) {
         }
     });
 
-    app.get('/reset', function(req, res) {
+    app.get('/sensors', function(req, res) {
         if (req.session.user == null) {
             res.redirect('/');
         } else {
-            AM.delAllRecords(function() {
-                res.redirect('/print');
-            });
+            //temporary object data, replace with database table
+            var sensorData = new Object({
+                "B8:27:EB:CE:93:69": {
+                    "name": "Sensor 2",
+                    "group": "Test Group 2",
+                    "location": "Melbourne",
+                    "long": "-80.6081",
+                    "lat": "28.0836",
+                    "status": "danger"
+                },
+                "B8:27:EB:DA:8F:F5": {
+                    "name": "Sensor 1",
+                    "group": "Test Group 1",
+                    "location": "Orlando",
+                    "long": "-81.4712688539999",
+                    "lat": "28.5224150600001",
+                    "status": "ok"
+                },
+                "B8:27:EB:97:19:1E": {
+                    "name": "Sensor 3",
+                    "group": "Test Group 1",
+                    "location": "Orlando",
+                    "long": "-81.4076",
+                    "lat": "28.2920",
+                    "status": "ok"
+                }
+            })
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(sensorData));
         }
     });
 
