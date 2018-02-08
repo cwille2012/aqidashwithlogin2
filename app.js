cbd3ce9324240892929f2d147588fba5a9e66033
Chris Wille <chris@christopherwille.com>
2018-02-08 04:17:31 -0500

    test seperation

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(session({
 }));
 
 require('./app/server/routes')(app);
+require('./app/server/handlers')(app);
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
