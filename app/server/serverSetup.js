dac95150746290a9a1c62fccbfdc7950c82a3814
Chris Wille <chris@christopherwille.com>
2018-02-12 09:10:31 -0500

    fix write issue

--- app/server/serverSetup.js
+++ app/server/serverSetup.js
@@ -7,16 +7,30 @@ var databaseURL = 'mongodb://' + databaseHost + ':' + databasePort + '/' + datab
 
 MongoClient.connect(databaseURL, function(err, db) {
     if (err) throw err;
-    console.log("Dashboard database created!");
+    console.log("Connected to Dashboard Database");
     var dbo = db.db("dashboard");
     dbo.createCollection("whitelist", function(err, res) {
         if (err) throw err;
         console.log("Whitelist collection created!");
     });
+    db.close();
+});
+
+MongoClient.connect(databaseURL, function(err, db) {
+    if (err) throw err;
+    console.log("Connected to Dashboard Database");
+    var dbo = db.db("dashboard");
     dbo.createCollection("dashsettings", function(err, res) {
         if (err) throw err;
         console.log("Dashboard settings collection created!");
     });
+    db.close();
+});
+
+MongoClient.connect(databaseURL, function(err, db) {
+    if (err) throw err;
+    console.log("Connected to Dashboard Database");
+    var dbo = db.db("dashboard");
     dbo.createCollection("sensors", function(err, res) {
         if (err) throw err;
         console.log("Sensor collection created!");
