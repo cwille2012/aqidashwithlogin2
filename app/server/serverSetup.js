0dd3af273d06b42ab9522a2ef47b6906747b10b8
Chris Wille <chris@christopherwille.com>
2018-02-12 09:06:18 -0500

    url fix

--- app/server/serverSetup.js
+++ app/server/serverSetup.js
@@ -5,7 +5,7 @@ var databasePort = 27017;
 var databaseName = "dashboard";
 var databaseURL = 'mongodb://' + databaseHost + ':' + databasePort + '/' + databaseName;
 
-MongoClient.connect(whitelistURL, function(err, db) {
+MongoClient.connect(databaseURL, function(err, db) {
     if (err) throw err;
     console.log("Dashboard database created!");
     var dbo = db.db("dashboard");
