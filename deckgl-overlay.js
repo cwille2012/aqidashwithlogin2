3f12ba7e952f024d86a0fdfbcf96dcfda562c0af
Chris Wille <chris@christopherwille.com>
2018-02-22 20:11:57 -0500

    icon update

--- deckgl-overlay.js
+++ deckgl-overlay.js
@@ -1,5 +1,5 @@
 /* global window */
-import React, {Component} from 'react';
+/*import React, {Component} from 'react';
 import DeckGL, {IconLayer, WebMercatorViewport} from 'deck.gl';
 import rbush from 'rbush';
 
@@ -155,3 +155,28 @@ export default class DeckGLOverlay extends Component {
     return <DeckGL {...viewport} layers={[layer]} />;
   }
 }
+*/
+
+
+const ICON_MAPPING = {
+    marker: { x: 0, y: 0, width: 32, height: 32, mask: true }
+};
+
+//var data = { position: [-122.4, 37.7], icon: 'marker', size: 24, color: [255, 0, 0] };
+
+const App = ({ data, viewport }) => {
+
+
+        const layer = new IconLayer({
+            id: 'icon-layer',
+            data,
+            iconAtlas: './mapdata/icon.png',
+            iconMapping: ICON_MAPPING
+        });
+
+        return ( < DeckGL {...viewport }
+            layers = {
+                [layer]
+            }
+            />);
+        };
