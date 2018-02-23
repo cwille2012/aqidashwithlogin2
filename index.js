5dabba6db57875794a3fcb919910b87ad5adefdf
Chris Wille <chris@christopherwille.com>
2018-02-22 19:42:39 -0500

    map changed to icons

--- index.js
+++ index.js
@@ -1,34 +1,81 @@
-import DeckGL, { IconLayer } from 'deck.gl';
+/* global window,document */
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import MapGL from 'react-map-gl';
+import DeckGLOverlay from './deckgl-overlay.js';
 
-const MAPBOX_TOKEN = "pk.eyJ1IjoiY3dpbGxlMjAxMiIsImEiOiJjajJxdWJyeXEwMDE5MzNydXF2cm1sbDU1In0.kCKIz6Ivh3EfNOmEfTANOA"; // eslint-disable-line
+import { json as requestJson } from 'd3-request';
 
+// Set your mapbox token here
+const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+
+// Source data CSV
 const DATA_URL =
-    'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/icon/meteorites.json';
+    'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/icon/meteorites.json'; // eslint-disable-line
 
-
-const ICON_MAPPING = {
-    marker: { x: 0, y: 0, width: 32, height: 32, mask: true }
-};
-
-const App = ({ data, viewport }) => {
-
-        /**
-         * Data format:
-         * [
-         *   {position: [-122.4, 37.7], icon: 'marker', size: 24, color: [255, 0, 0]},
-         *   ...
-         * ]
-         */
-        const layer = new IconLayer({
-            id: 'icon-layer',
-            data,
-            iconAtlas: '/app/public/mapdata/icon.png',
-            iconMapping: ICON_MAPPING
-        });
-
-        return ( < DeckGL {...viewport }
-            layers = {
-                [layer]
-            }
-            />);
+class Root extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                ...DeckGLOverlay.defaultViewport,
+                width: 500,
+                height: 500
+            },
+            data: null,
+            iconMapping: null
         };
+
+        requestJson(DATA_URL, (error, response) => {
+            if (!error) {
+                this.setState({ data: response });
+            }
+        });
+        requestJson('./public/mapdata/location-icon-mapping.json', (error, response) => {
+            if (!error) {
+                this.setState({ iconMapping: response });
+            }
+        });
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this._resize.bind(this));
+        this._resize();
+    }
+
+    _resize() {
+        this._onViewportChange({
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+    }
+
+    _onViewportChange(viewport) {
+        this.setState({
+            viewport: {...this.state.viewport, ...viewport }
+        });
+    }
+
+    render() {
+        const { viewport, data, iconMapping } = this.state;
+
+        return ( <
+            MapGL {...viewport }
+            mapStyle = "mapbox://styles/mapbox/dark-v9"
+            onViewportChange = { this._onViewportChange.bind(this) }
+            mapboxApiAccessToken = { MAPBOX_TOKEN } >
+            <
+            DeckGLOverlay viewport = { viewport }
+            data = { data }
+            iconAtlas = "data/location-icon-atlas.png"
+            iconMapping = { iconMapping }
+            showCluster = { true }
+            /> < /
+            MapGL >
+        );
+    }
+}
+
+if (!!document.getElementById('mapHolder')) {
+    render( < Root / > , document.getElementById('mapHolder').appendChild(document.createElement('div')));
+}
