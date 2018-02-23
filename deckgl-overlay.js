8465cb9d23fb385134702a589492db6d8c564ed6
Chris Wille <chris@christopherwille.com>
2018-02-22 19:36:09 -0500

    map update

--- deckgl-overlay.js
+++ deckgl-overlay.js
@@ -1,60 +1,160 @@
-import React, {Component} from 'react';
-import DeckGL, {PolygonLayer} from 'deck.gl';
-import TripsLayer from './trips-layer';
+/* global window */
+import React, { Component } from 'react';
+import DeckGL, { IconLayer, WebMercatorViewport } from 'deck.gl';
+import rbush from 'rbush';
 
-const LIGHT_SETTINGS = {
-  lightsPosition: [-74.05, 40.7, 8000, -73.5, 41, 5000],
-  ambientRatio: 0.05,
-  diffuseRatio: 0.6,
-  specularRatio: 0.8,
-  lightsStrength: [2.0, 0.0, 0.0, 0.0],
-  numberOfLights: 2
-};
+const ICON_SIZE = 60;
+
+function getIconName(size) {
+    if (size === 0) {
+        return '';
+    }
+    if (size < 10) {
+        return `marker-${size}`;
+    }
+    if (size < 100) {
+        return `marker-${Math.floor(size / 10)}0`;
+    }
+    return 'marker-100';
+}
+
+function getIconSize(size) {
+    return Math.min(100, size) / 100 * 0.5 + 0.5;
+}
 
 export default class DeckGLOverlay extends Component {
-  static get defaultViewport() {
-    return {
-      longitude: -74,
-      latitude: 40.72,
-      zoom: 13,
-      maxZoom: 16,
-      pitch: 45,
-      bearing: 0
-    };
-  }
-
-  render() {
-    const {viewport, buildings, trips, trailLength, time} = this.props;
-
-    if (!buildings || !trips) {
-      return null;
+    static get defaultViewport() {
+        return {
+            longitude: -35,
+            latitude: 36.7,
+            zoom: 1.8,
+            maxZoom: 20,
+            pitch: 0,
+            bearing: 0
+        };
     }
 
-    const layers = [
-      new TripsLayer({
-        id: 'trips',
-        data: trips,
-        getPath: d => d.segments,
-        getColor: d => (d.vendor === 0 ? [253, 128, 93] : [23, 184, 190]),
-        opacity: 0.3,
-        strokeWidth: 2,
-        trailLength,
-        currentTime: time
-      }),
-      new PolygonLayer({
-        id: 'buildings',
-        data: buildings,
-        extruded: true,
-        wireframe: false,
-        fp64: true,
-        opacity: 0.5,
-        getPolygon: f => f.polygon,
-        getElevation: f => f.height,
-        getFillColor: f => [74, 80, 87],
-        lightSettings: LIGHT_SETTINGS
-      })
-    ];
+    constructor(props) {
+        super(props);
 
-    return <DeckGL {...viewport} layers={layers} />;
-  }
+        // build spatial index
+        this._tree = rbush(9, ['.x', '.y', '.x', '.y']);
+        this.state = {
+            x: 0,
+            y: 0,
+            hoveredItems: null,
+            expanded: false
+        };
+
+        this._updateCluster(props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const { viewport } = nextProps;
+        const oldViewport = this.props.viewport;
+
+        if (
+            nextProps.data !== this.props.data ||
+            viewport.width !== oldViewport.width ||
+            viewport.height !== oldViewport.height
+        ) {
+            this._updateCluster(nextProps);
+        }
+    }
+
+    // Compute icon clusters
+    // We use the projected positions instead of longitude and latitude to build
+    // the spatial index, because this particular dataset is distributed all over
+    // the world, we can't use some fixed deltaLon and deltaLat
+    _updateCluster({ data, viewport }) {
+        if (!data) {
+            return;
+        }
+
+        const tree = this._tree;
+
+        const transform = new WebMercatorViewport({
+            ...viewport,
+            zoom: 0
+        });
+
+        data.forEach(p => {
+            const screenCoords = transform.project(p.coordinates);
+            p.x = screenCoords[0];
+            p.y = screenCoords[1];
+            p.zoomLevels = [];
+        });
+
+        tree.clear();
+        tree.load(data);
+
+        for (let z = 0; z <= 20; z++) {
+            const radius = ICON_SIZE / 2 / Math.pow(2, z);
+
+            data.forEach(p => {
+                if (p.zoomLevels[z] === undefined) {
+                    // this point does not belong to a cluster
+                    const { x, y } = p;
+
+                    // find all points within radius that do not belong to a cluster
+                    const neighbors = tree
+                        .search({
+                            minX: x - radius,
+                            minY: y - radius,
+                            maxX: x + radius,
+                            maxY: y + radius
+                        })
+                        .filter(neighbor => neighbor.zoomLevels[z] === undefined);
+
+                    // only show the center point at this zoom level
+                    neighbors.forEach(neighbor => {
+                        if (neighbor === p) {
+                            p.zoomLevels[z] = {
+                                icon: getIconName(neighbors.length),
+                                size: getIconSize(neighbors.length),
+                                points: neighbors
+                            };
+                        } else {
+                            neighbor.zoomLevels[z] = null;
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    render() {
+        const { viewport, data, iconAtlas, iconMapping, showCluster } = this.props;
+
+        if (!data || !iconMapping) {
+            return null;
+        }
+
+        const z = Math.floor(viewport.zoom);
+        const size = showCluster ? 1 : Math.min(Math.pow(1.5, viewport.zoom - 10), 1);
+        const updateTrigger = z * showCluster;
+
+        const layer = new IconLayer({
+            id: 'icon',
+            data,
+            pickable: this.props.onHover || this.props.onClick,
+            iconAtlas,
+            iconMapping,
+            sizeScale: ICON_SIZE * size * window.devicePixelRatio,
+            getPosition: d => d.coordinates,
+            getIcon: d => (showCluster ? d.zoomLevels[z] && d.zoomLevels[z].icon : 'marker'),
+            getSize: d => (showCluster ? d.zoomLevels[z] && d.zoomLevels[z].size : 1),
+            onHover: this.props.onHover,
+            onClick: this.props.onClick,
+            updateTriggers: {
+                getIcon: updateTrigger,
+                getSize: updateTrigger
+            }
+        });
+
+        return <DeckGL {...viewport }
+        layers = {
+            [layer] }
+        />;
+    }
 }
--- index.js
+++ index.js
@@ -1,102 +1,34 @@
-/* global window,document */
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import MapGL from 'react-map-gl';
-import DeckGLOverlay from './deckgl-overlay.js';
+import DeckGL, { IconLayer } from 'deck.gl';
 
-import { json as requestJson } from 'd3-request';
-
-// Set your mapbox token here
 const MAPBOX_TOKEN = "pk.eyJ1IjoiY3dpbGxlMjAxMiIsImEiOiJjajJxdWJyeXEwMDE5MzNydXF2cm1sbDU1In0.kCKIz6Ivh3EfNOmEfTANOA"; // eslint-disable-line
 
-// Source data CSV
-const DATA_URL = {
-    BUILDINGS: 'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/trips/buildings.json', // eslint-disable-line
-    TRIPS: 'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/trips/trips.json' // eslint-disable-line
+const DATA_URL =
+    'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/icon/meteorites.json';
+
+
+const ICON_MAPPING = {
+    marker: { x: 0, y: 0, width: 32, height: 32, mask: true }
 };
 
-class Root extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            viewport: {
-                ...DeckGLOverlay.defaultViewport,
-                width: 500,
-                height: 500
-            },
-            buildings: null,
-            trips: null,
-            time: 0
+const App = ({ data, viewport }) => {
+
+        /**
+         * Data format:
+         * [
+         *   {position: [-122.4, 37.7], icon: 'marker', size: 24, color: [255, 0, 0]},
+         *   ...
+         * ]
+         */
+        const layer = new IconLayer({
+            id: 'icon-layer',
+            data,
+            iconAtlas: '/app/public/mapdata/icon.png',
+            iconMapping: ICON_MAPPING
+        });
+
+        return ( < DeckGL {...viewport }
+            layers = {
+                [layer]
+            }
+            />);
         };
-
-        requestJson(DATA_URL.BUILDINGS, (error, response) => {
-            if (!error) {
-                this.setState({ buildings: response });
-            }
-        });
-
-        requestJson(DATA_URL.TRIPS, (error, response) => {
-            if (!error) {
-                this.setState({ trips: response });
-            }
-        });
-    }
-
-    componentDidMount() {
-        window.addEventListener('resize', this._resize.bind(this));
-        this._resize();
-        this._animate();
-    }
-
-    componentWillUnmount() {
-        if (this._animationFrame) {
-            window.cancelAnimationFrame(this._animationFrame);
-        }
-    }
-
-    _animate() {
-        const timestamp = Date.now();
-        const loopLength = 1800;
-        const loopTime = 60000;
-
-        this.setState({
-            time: (timestamp % loopTime) / loopTime * loopLength
-        });
-        this._animationFrame = window.requestAnimationFrame(this._animate.bind(this));
-    }
-
-    _resize() {
-        this._onViewportChange({
-            width: window.innerWidth,
-            height: window.innerHeight
-        });
-    }
-
-    _onViewportChange(viewport) {
-        this.setState({
-            viewport: {...this.state.viewport, ...viewport }
-        });
-    }
-
-    render() {
-        const { viewport, buildings, trips, time } = this.state;
-
-        return ( <
-            MapGL {...viewport }
-            mapStyle = "mapbox://styles/mapbox/dark-v9"
-            onViewportChange = { this._onViewportChange.bind(this) }
-            mapboxApiAccessToken = { MAPBOX_TOKEN } >
-            <
-            DeckGLOverlay viewport = { viewport }
-            buildings = { buildings }
-            trips = { trips }
-            trailLength = { 180 }
-            time = { time }
-            /> < /
-            MapGL >
-        );
-    }
-}
-if (!!document.getElementById('mapHolder')) {
-    render( < Root / > , document.getElementById('mapHolder').appendChild(document.createElement('div')));
-}
